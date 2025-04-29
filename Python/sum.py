20d8d41b87c46d06874923b2dad19357a8ecf438
AYAELJEMLI <ayaeljemli5@gmail.com>
2025-04-29 17:28:13 +0100

    Update sum.py

--- Python/sum.py
+++ Python/sum.py
@@ -8,7 +8,7 @@ def sum_file(filename):
             total += sum(int(number) for number in numbers)  
     return total
 
-#filen = r"C:\Users\Belrayy\Documents\EMSI\Coursera\Python\regex_sum_42.txt" the answer is 445833
-filen = r"C:\Users\Belrayy\Documents\EMSI\Coursera\Python\regex_sum_.txt"
+#filen = r"http://py4e-data.dr-chuck.net/regex_sum_42.txt" the answer is 445833
+filen = r"http://py4e-data.dr-chuck.net/regex_sum_2216923.txt"
 result = sum_file(filen)
 print(f"The sum of all numbers in the file is: {result}")
